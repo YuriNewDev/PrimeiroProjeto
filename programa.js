a7000c3ff540a63cc409491975e9a0e09aa3ec99
YuriNewDev <166752771+YuriNewDev@users.noreply.github.com>
2024-09-11 15:08:18 -0300

    Update programa.js

--- programa.js
+++ programa.js
@@ -1,9 +1,9 @@
 
 
 function idade(){
-
-    var ano, calculo;
-    var resultado; 
+    var = calculo
+    var = ano, ;
+    var = resultado; 
     ano = document.getElementById("idade1").value;
     calculo= 2024 - ano;
     document.getElementById("resultado").innerHTML = calculo;
